2251ae1ebe9d372c86a464a8bed96a5e9d563b4b
Miguel Angel Kantun Huchin <132594524+miguelkh@users.noreply.github.com>
2025-08-28 12:03:41 -0600

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/miguelkh/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('miguelkh')
+            node / gitConfigEmail('miguelkh21@gmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('miguelkh/nodejsapp')
             tag('${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
+            registryCredentials('docker-hub-prueba')
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
